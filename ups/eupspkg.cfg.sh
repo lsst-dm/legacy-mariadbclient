e04ec588906b4e54ee1b13369ccbc4824fa4c91f
Joshua Hoblitt <josh@hoblitt.com>
2016-02-01 17:01:59 -0700

    sync eupspkg.cfg.sh with lsst/mariadb

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -2,6 +2,13 @@
 
 config()
 {
+    # Disable external SSL on OS X as it is not a standard library
+    # and LSST does not need full SSL support.
+    ssloptions=""
+    if [[ $(uname) == Darwin ]]; then
+        ssloptions="-DWITH_SSL=bundled"
+    fi
+
     # TokuDB requires third-party software jemalloci (see http://www.canonware.com/jemalloc/)
-    cmake . -DCMAKE_INSTALL_PREFIX=${PREFIX} -DPLUGIN_TOKUDB=NO -DWITHOUT_SERVER=ON
+    cmake . -DCMAKE_INSTALL_PREFIX=${PREFIX} -DPLUGIN_TOKUDB=NO $ssloptions -DWITHOUT_SERVER=ON
 }
