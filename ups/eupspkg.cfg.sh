bf35b606033e2b9b90053ac5d40ef27b2bb03370
Joshua Hoblitt <josh@hoblitt.com>
2016-02-22 17:36:26 -0700

    update to work with mariadb[client] 10.1.x

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -1,14 +1,36 @@
-# EupsPkg config file. Sourced by 'eupspkg'
+#!/usr/bin/env bash
 
 config()
 {
-    # Disable external SSL on OS X as it is not a standard library
-    # and LSST does not need full SSL support.
-    ssloptions=""
-    if [[ $(uname) == Darwin ]]; then
-        ssloptions="-DWITH_SSL=bundled"
-    fi
 
+    ARGS=()
+
+    ARGS+=("-DCMAKE_INSTALL_PREFIX=${PREFIX}")
     # TokuDB requires third-party software jemalloci (see http://www.canonware.com/jemalloc/)
-    cmake . -DCMAKE_INSTALL_PREFIX=${PREFIX} -DPLUGIN_TOKUDB=NO $ssloptions -DWITHOUT_SERVER=ON
+    ARGS+=('-DPLUGIN_TOKUDB=NO')
+
+    case $(uname) in
+        Linux*)
+            if grep -q -i "CentOS release 5" /etc/redhat-release; then
+                # mroonga does not build on EL5 (conda ref platform) and is
+                # unused by LSST
+                ARGS+=('-DPLUGIN_MROONGA=NO')
+                # required to build on EL5
+                ARGS+=('-DHAVE_CXX_NEW=NO')
+            fi
+            ;;
+        Darwin*)
+            # Disable external SSL on OS X as it is not a standard library
+            # and LSST does not need full SSL support.
+            ARGS+=('-DWITH_SSL=bundled')
+            ;;
+        *)
+            # non-fatal
+            echo "unsupported platform: $(uname)"
+            ;;
+    esac
+
+    ARGS+=('-DWITHOUT_SERVER=ON')
+
+    cmake . "${ARGS[@]}"
 }
